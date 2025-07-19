85a5e32f545076e9c7f2111b4d246930464a9e94
ShowMeJoy <showmejoy@icloud.com>
2025-07-19 19:27:28 +0300

    Update backend main.py, add log

--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,44 @@
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 
-app = FastAPI()
+# uvicorn main:app --host 0.0.0.0 --port 8000
 
 
 class SensorData(BaseModel):
+    device_id: str = 'esp32-1'
     temperature: float
     humidity: float
     timestamp: str
 
 
-@app.post("/log")
+app = FastAPI()
+data_log: list[SensorData] = []
+log_message: list[str] = []
+
+
+@app.post("/log/")
 def receive_data(data: SensorData):
+    log_entry = f"Device={data.device_id} | Temp={data.temperature} C | Hum={data.humidity} | Time={data.timestamp}"
+    log_message.append(log_entry)
+    if data.temperature > 40:
+        return JSONResponse(
+            status_code=400,
+            content={"error": "Overheat detected"}
+        )
+    data_log.append(data)
     print(f"{data.timestamp}: {data.temperature} C, {data.humidity} %")
-    return {"status": "ok"}
+    return JSONResponse(
+        status_code=201,
+        content={"status": "ok"}
+    )
+
+
+@app.get("/log/")
+def get_log():
+    return data_log
+
+
+@app.get("/log/message")
+def get_message():
+    return log_message
