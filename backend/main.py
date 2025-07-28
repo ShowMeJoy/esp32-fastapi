ebf6028e33ba5048ce83e995ac28cd68f0bc3f83
ShowMeJoy <showmejoy@icloud.com>
2025-07-28 15:43:49 +0300

    update main.py, add SQL logging

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
-from fastapi.responses import JSONResponse
+from fastapi.responses import JSONResponse  # for work with status coded
 from pydantic import BaseModel
+from typing import Optional, List   # specific types
+from sqlmodel import Field, SQLModel, create_engine, Session, select
 
 # uvicorn main:app --host 0.0.0.0 --port 8000
 
@@ -9,7 +11,19 @@ class SensorData(BaseModel):
     device_id: str = 'esp32-1'
     temperature: float
     humidity: float
-    timestamp: str
+    timestamp: int
+
+
+class Measurement(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    device_id: str
+    temperature: float
+    humidity: float
+    timestamp: int
+
+
+# SQLite file
+engine = create_engine("sqlite:///database.db")
 
 
 app = FastAPI()
@@ -17,28 +31,24 @@ data_log: list[SensorData] = []
 log_message: list[str] = []
 
 
-@app.post("/log/")
-def receive_data(data: SensorData):
-    log_entry = f"Device={data.device_id} | Temp={data.temperature} C | Hum={data.humidity} | Time={data.timestamp}"
-    log_message.append(log_entry)
-    if data.temperature > 40:
-        return JSONResponse(
-            status_code=400,
-            content={"error": "Overheat detected"}
-        )
-    data_log.append(data)
-    print(f"{data.timestamp}: {data.temperature} C, {data.humidity} %")
-    return JSONResponse(
-        status_code=201,
-        content={"status": "ok"}
-    )
+# create datasheet
+@app.on_event("startup")
+def on_startup():
+    SQLModel.metadata.create_all(engine)
 
 
-@app.get("/log/")
+# POST: add update
+@app.post("/submit/")
+def submit_data(measurement: Measurement):
+    with Session(engine) as session:
+        session.add(measurement)
+        session.commit()
+    return {"status": "saved"}
+
+
+# GET: return all
+@app.get("/log/", response_model=List[Measurement])
 def get_log():
-    return data_log
-
-
-@app.get("/log/message")
-def get_message():
-    return log_message
+    with Session(engine) as session:
+        result = session.exec(select(Measurement)).all()
+        return result
